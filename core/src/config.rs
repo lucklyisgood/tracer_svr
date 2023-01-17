34eafdf6772e28b7b71df94a24f04d5f30d89e32
vker <2807582398@qq.com>
2023-01-18 01:20:45 +0800

    add print db url log

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,7 +1,7 @@
-use std::env;
 use dotenv::dotenv;
 use once_cell::sync::OnceCell;
 use sea_orm::ConnectOptions;
+use std::env;
 
 pub static APP_CFG: OnceCell<AppConfig> = OnceCell::new();
 
@@ -14,6 +14,7 @@ pub struct AppConfig {
 
 pub fn create_db_cfg() -> ConnectOptions {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    println!("log:: db_url: {}", db_url);
     let max_connections: u32 = env::var("MAX_CONNECTIONS").expect("").parse().expect("");
     let mut opts = ConnectOptions::new(db_url);
     opts.max_connections(max_connections);
@@ -22,11 +23,13 @@ pub fn create_db_cfg() -> ConnectOptions {
 
 pub fn setup() -> std::io::Result<()> {
     dotenv().ok();
-    
+
     let host = env::var("HOST").expect("HOST is not set in .env file");
     let port = env::var("PORT").expect("PORT is not set in .env file");
     let db = create_db_cfg();
 
-    APP_CFG.set(AppConfig {host, port, db}).expect("App Config set err");
+    APP_CFG
+        .set(AppConfig { host, port, db })
+        .expect("App Config set err");
     Ok(())
 }
